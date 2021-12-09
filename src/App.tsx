7e2e25374adbdced9d5163fcc53eb5ea3346a253
Zansuken <doom.sebastien.pro@gmail.com>
2021-12-09 10:53:09 +0100

    feat(home page): Created files and first component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.scss';
 import { slider } from './components/slider';
+import { buildHomePage } from './components/home-page';
 
 function App() {
   return (
     <div className="App">
+      <main>
+        {buildHomePage()}
+      </main>
       <footer className="app_footer">
         {slider()}
       </footer>
---
+++ src/components/home-page.tsx
@@ -0,0 +1,16 @@
+import './home-page.scss'
+
+export function buildHomePage () {
+    return (
+        <div className="home_layout">
+            <div className="home_header">
+            <h1>
+                Welcome to Zansuken's Website.
+            </h1>
+            <h3>
+                Created with React, Typescript and SASS... 
+            </h3>
+            </div>
+        </div>
+    )
+}
