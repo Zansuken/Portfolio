912483c5774e8a25b14c727a91695607c9dbfa8f
Zansuken <doom.sebastien.pro@gmail.com>
2021-12-18 19:27:25 +0100

    feat(infos): added hover effect on labels

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,3 +1,38 @@
-export function Resume() {
-    
+import { useState } from "react"
+import '../sass/Resume.scss';
+
+export default function Resume() {
+
+    const [visibility, setVisibility] = useState(true)
+
+    function hideSection() {
+        visibility? setVisibility(false):setVisibility(true)
+    }
+
+    return (
+        <section className={visibility?'resume':'resume-hidden'}>
+            <span className='section_label' onClick={() => {
+                hideSection()
+            }}>RESUME</span>
+            <h2>Resume</h2>
+            <h3>Experiences</h3>
+            <ul>
+                <li><div><h4>Dealer at <a href="https://casinodespa.be">Casino de Spa</a></h4><legend>From **** to ****</legend></div></li>
+            </ul>
+            <h3>Soft skills</h3>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <h3>Hard skills</h3>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
+    )
 }
