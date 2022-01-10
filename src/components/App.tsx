df3abfb566a2a853d866ee3e1bcf2ce4ec5ad6dd
Zansuken <doom.sebastien.pro@gmail.com>
2022-01-10 22:24:56 +0100

    feat(project): added a mobile project preview

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import { Slider } from './slider';
 import { HomePage } from './home-page';
 import { ProjectView } from './project';
 import { useState } from 'react';
+import { SliderData } from './sliderData';
 
 function App() {
 
@@ -13,6 +14,17 @@ function App() {
       <main>
         <HomePage />
         { view !== null && <ProjectView onClick={() => setView(null)} index={view} />}
+        <button className="mobile_button">Projects</button>
+        {SliderData.map((slide, index) => {
+                    return (
+                        <div className="project_mobile_thumbnail" key={index}>
+                            <div onClick={() => setView(index)}>
+                                <img src={slide.thumbnail} alt={slide.name} />
+                                <span>{slide.name}</span>
+                            </div>
+                        </div>
+                    )
+                })}
       </main>
       <Slider onClick={(index) => setView(index)}/>
     </div>
