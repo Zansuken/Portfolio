70bc347b4417195a2b713464140068d5cfa48d68
Zansuken <doom.sebastien.pro@gmail.com>
2022-01-20 18:28:51 +0100

    fix(project-view): fixed some bugs on frames view

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,18 +1,19 @@
 import '../sass/project.scss'
 import { SliderData } from './sliderData'
-import {GrFormClose} from 'react-icons/gr'
+import { GrFormClose } from 'react-icons/gr'
 import { FC } from 'react'
 
-export const ProjectView :FC <{index: number, onClick: () => void }> = (props) => {
+export const ProjectView: FC<{ index: number, onClick: () => void }> = (props) => {
 
         return (
-    
+
                 <div className='projectView'>
-    
-                <button onClick={() => props.onClick()}><GrFormClose /></button>
-                <iframe src={SliderData[props.index].url} frameBorder="0" title={SliderData[props.index].name}></iframe>
+
+                        <button onClick={() => props.onClick()}><GrFormClose /></button>
+                        <iframe src={SliderData[props.index].url} frameBorder="0" title={SliderData[props.index].name}></iframe>
+                        <a style={{ display: "table-cell" }} href={SliderData[props.index].url} target="_blank" rel="noopener noreferrer"> Open in a new tab</a>
                 </div>
-    
+
         )
 
 }
